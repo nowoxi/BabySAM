7cf53822f0758965997d5e945442275986a4cf7c
nowoxi <nowoxi@yahoo.com>
2011-05-14 23:04:03 +0100

    modified:   bin/BabySAM.apk 	modified:   bin/classes.dex 	modified:   bin/com/androidproject/babysam/EventActivity.class 	modified:   bin/com/androidproject/babysam/FaceActivity.class 	modified:   bin/com/androidproject/babysam/HelpActivity.class 	modified:   bin/com/androidproject/babysam/LoginActivity.class 	modified:   bin/com/androidproject/babysam/MenuActivity$1.class 	modified:   bin/com/androidproject/babysam/MenuActivity.class 	modified:   bin/com/androidproject/babysam/OfficalsActivity.class 	modified:   bin/com/androidproject/babysam/OldActivity.class 	modified:   bin/com/androidproject/babysam/R$color.class 	modified:   bin/com/androidproject/babysam/R$dimen.class 	modified:   bin/com/androidproject/babysam/R$drawable.class 	modified:   bin/com/androidproject/babysam/R$id.class 	modified:   bin/com/androidproject/babysam/R$layout.class 	modified:   bin/com/androidproject/babysam/R$menu.class 	modified:   bin/com/androidproject/babysam/R$string.class 	modified:   bin/com/androidproject/babysam/R.class 	modified:   bin/com/androidproject/babysam/SessionActivity.class 	modified:   bin/com/androidproject/babysam/SplashActivity.class 	modified:   bin/com/androidproject/babysam/StudentsActivity.class 	modified:   bin/resources.ap_ 	modified:   gen/com/androidproject/babysam/R.java 	deleted:    res/drawable-hdpi/babysam_icon.png 	deleted:    res/drawable-hdpi/icon.png 	deleted:    res/drawable-ldpi/babysam_icon.png 	deleted:    res/drawable-ldpi/icon.png 	deleted:    res/drawable-mdpi/babysam_icon.png 	deleted:    res/drawable-mdpi/icon.png 	modified:   res/layout/event.xml 	modified:   res/layout/face.xml 	modified:   res/layout/help.xml 	modified:   res/layout/menu_item.xml 	modified:   res/layout/old.xml 	modified:   res/layout/old_display.xml 	modified:   res/values/strings.xml 	modified:   src/com/androidproject/babysam/HelpActivity.java

--- gen/com/androidproject/babysam/R.java
+++ gen/com/androidproject/babysam/R.java
@@ -16,43 +16,60 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int menu_color=0x7f050004;
-        public static final int menu_glow=0x7f050005;
-        public static final int title_color=0x7f050000;
-        public static final int title_glow=0x7f050003;
-        public static final int vback=0x7f050002;
-        public static final int vtext=0x7f050001;
+        public static final int menu_color=0x7f060004;
+        public static final int menu_glow=0x7f060005;
+        public static final int title_color=0x7f060000;
+        public static final int title_glow=0x7f060003;
+        public static final int vback=0x7f060002;
+        public static final int vtext=0x7f060001;
     }
     public static final class dimen {
-        public static final int menu_item_size=0x7f060003;
-        public static final int menu_item_top=0x7f060004;
-        public static final int title_font=0x7f060000;
-        public static final int vfont=0x7f060001;
-        public static final int vspace=0x7f060002;
+        public static final int menu_item_size=0x7f070003;
+        public static final int menu_item_top=0x7f070004;
+        public static final int title_font=0x7f070000;
+        public static final int vfont=0x7f070001;
+        public static final int vspace=0x7f070002;
     }
     public static final class drawable {
         public static final int babysam_icon=0x7f020000;
-        public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int TextViewBottomVersion=0x7f090006;
-        public static final int auth_but=0x7f090001;
-        public static final int event_addof=0x7f09000d;
-        public static final int event_addse=0x7f09000c;
-        public static final int event_addst=0x7f09000e;
-        public static final int event_cancel=0x7f090010;
-        public static final int event_help=0x7f09000b;
-        public static final int event_save=0x7f09000f;
-        public static final int help_menu_item=0x7f090011;
-        public static final int imageView1=0x7f090005;
-        public static final int logo=0x7f090007;
-        public static final int menu_list=0x7f090004;
-        public static final int menu_text=0x7f090003;
-        public static final int relativeLayout1=0x7f090000;
-        public static final int relativeLayout2=0x7f090002;
-        public static final int textView1=0x7f090008;
-        public static final int zxing_but=0x7f090009;
-        public static final int zxing_result=0x7f09000a;
+        public static final int TextView01=0x7f0a0006;
+        public static final int TextView02=0x7f0a0005;
+        public static final int TextView03=0x7f0a000c;
+        public static final int TextViewBottomVersion=0x7f0a0019;
+        public static final int TextView_HelpText=0x7f0a0011;
+        public static final int auth_but=0x7f0a0013;
+        public static final int event_addof=0x7f0a001f;
+        public static final int event_addse=0x7f0a001e;
+        public static final int event_addst=0x7f0a0020;
+        public static final int event_cancel=0x7f0a0022;
+        public static final int event_help=0x7f0a001d;
+        public static final int event_save=0x7f0a0021;
+        public static final int help_menu_item=0x7f0a0023;
+        public static final int imageView1=0x7f0a0016;
+        public static final int linearLayout1=0x7f0a000a;
+        public static final int linearLayout2=0x7f0a000f;
+        public static final int listView1=0x7f0a0017;
+        public static final int listView2=0x7f0a0018;
+        public static final int logo=0x7f0a001a;
+        public static final int menu_list=0x7f0a0015;
+        public static final int menu_text=0x7f0a0014;
+        public static final int officials_view=0x7f0a000b;
+        public static final int relativeLayout1=0x7f0a0012;
+        public static final int relativeLayout2=0x7f0a0008;
+        public static final int relativeLayout3=0x7f0a000d;
+        public static final int scrollView1=0x7f0a0009;
+        public static final int scrollView2=0x7f0a000e;
+        public static final int students_view=0x7f0a0010;
+        public static final int tableLayout1=0x7f0a0000;
+        public static final int tableRow1=0x7f0a0001;
+        public static final int tableRow2=0x7f0a0004;
+        public static final int textView1=0x7f0a0002;
+        public static final int textView2=0x7f0a0003;
+        public static final int textView3=0x7f0a0007;
+        public static final int zxing_but=0x7f0a001b;
+        public static final int zxing_result=0x7f0a001c;
     }
     public static final class layout {
         public static final int event=0x7f030000;
@@ -70,39 +87,44 @@ public final class R {
         public static final int students=0x7f03000c;
     }
     public static final class menu {
-        public static final int eventoptions=0x7f080000;
-        public static final int faceoptions=0x7f080001;
-        public static final int officials_options=0x7f080002;
-        public static final int oldoptions=0x7f080003;
-        public static final int session_options=0x7f080004;
-        public static final int students_options=0x7f080005;
+        public static final int eventoptions=0x7f090000;
+        public static final int faceoptions=0x7f090001;
+        public static final int officials_options=0x7f090002;
+        public static final int oldoptions=0x7f090003;
+        public static final int session_options=0x7f090004;
+        public static final int students_options=0x7f090005;
+    }
+    public static final class raw {
+        public static final int help=0x7f050000;
     }
     public static final class string {
-        public static final int app_name=0x7f070000;
-        public static final int auth_but=0x7f07000e;
-        public static final int event=0x7f070004;
-        public static final int face=0x7f070007;
-        public static final int facerec_display=0x7f07000c;
-        public static final int help=0x7f070006;
-        public static final int login=0x7f070002;
-        public static final int menu=0x7f070003;
-        public static final int menu_item_event=0x7f070010;
-        public static final int menu_item_facerec=0x7f070012;
-        public static final int menu_item_help=0x7f070013;
-        public static final int menu_item_old=0x7f070011;
-        public static final int officials=0x7f070009;
-        public static final int old=0x7f070005;
-        public static final int old_display=0x7f07000b;
-        public static final int option_event_addof=0x7f070016;
-        public static final int option_event_addse=0x7f070015;
-        public static final int option_event_addst=0x7f070017;
-        public static final int option_event_cancel=0x7f070019;
-        public static final int option_event_save=0x7f070018;
-        public static final int option_help=0x7f070014;
-        public static final int session=0x7f070008;
-        public static final int splash=0x7f070001;
-        public static final int students=0x7f07000a;
-        public static final int version=0x7f07000d;
-        public static final int zxing_but=0x7f07000f;
+        public static final int app_name=0x7f080000;
+        public static final int auth_but=0x7f08000e;
+        public static final int event=0x7f080004;
+        public static final int event_offlabel=0x7f08001a;
+        public static final int event_stdlabel=0x7f08001b;
+        public static final int face=0x7f080007;
+        public static final int facerec_display=0x7f08000c;
+        public static final int help=0x7f080006;
+        public static final int login=0x7f080002;
+        public static final int menu=0x7f080003;
+        public static final int menu_item_event=0x7f080010;
+        public static final int menu_item_facerec=0x7f080012;
+        public static final int menu_item_help=0x7f080013;
+        public static final int menu_item_old=0x7f080011;
+        public static final int officials=0x7f080009;
+        public static final int old=0x7f080005;
+        public static final int old_display=0x7f08000b;
+        public static final int option_event_addof=0x7f080016;
+        public static final int option_event_addse=0x7f080015;
+        public static final int option_event_addst=0x7f080017;
+        public static final int option_event_cancel=0x7f080019;
+        public static final int option_event_save=0x7f080018;
+        public static final int option_help=0x7f080014;
+        public static final int session=0x7f080008;
+        public static final int splash=0x7f080001;
+        public static final int students=0x7f08000a;
+        public static final int version=0x7f08000d;
+        public static final int zxing_but=0x7f08000f;
     }
 }
--- src/com/androidproject/babysam/HelpActivity.java
+++ src/com/androidproject/babysam/HelpActivity.java
@@ -1,7 +1,12 @@
 package com.androidproject.babysam;
 
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class HelpActivity extends babysamActivity {
     /** Called when the activity is first created. */
@@ -9,5 +14,26 @@ public class HelpActivity extends babysamActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.help);
+        try {   
+            InputStream iFile = getResources().openRawResource(R.raw.help);
+            TextView helpText = (TextView) findViewById(R.id.TextView_HelpText);
+            String strFile = inputStreamToString(iFile);
+            helpText.setText(strFile);
+        } catch (Exception e) {
+            Log.e(TAG, "InputStreamToString failure", e);
+        }
+
+    }
+
+    public String inputStreamToString(InputStream is) throws IOException {
+            StringBuffer sBuffer = new StringBuffer();
+            DataInputStream dataIO = new DataInputStream(is);
+            String strLine = null;
+            while ((strLine = dataIO.readLine()) != null) {
+                sBuffer.append(strLine + "\n");
+            }
+            dataIO.close();
+            is.close();
+            return sBuffer.toString();
     }
 }
